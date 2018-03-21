7ffa88a8505e6513a5398e65ec5089a27ffd0c48
Ricardo Rokutan <ricardo.rokutan@gmail.com>
2018-03-21 17:46:39 -0300

    Initial commit

---
+++ Assets/4-Implementation/Scripts/Loader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour {
+
+	public void carregar1()
+	{
+		SceneManager.LoadScene ("demo_chasing");
+	}
+
+	public void carregar2()
+	{
+		SceneManager.LoadScene ("demo_scaping2");
+	}
+
+	public void carregar0()
+	{
+		SceneManager.LoadScene ("loader");
+	}
+}
